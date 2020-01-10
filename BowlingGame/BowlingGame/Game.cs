7071e864db2311c5106c3ed72169dc9467c80561
anisbastide <anis.bastide@upslide.net>
2020-01-10 17:29:30 +0100

    Started the kata

---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BowlingGame
+
+{
+    public class Game
+    {
+        private int score;
+        private int rollCount;
+        private int pinCount;
+        public void Roll(int knockedPins)
+        {
+            if (knockedPins == 20)
+            {
+
+            }
+            if (rollCount == 1)
+            {
+                
+                rollCount = 2;
+            }
+
+            if (rollCount == 2)
+            {
+                rollCount = 1;
+                score += Math.Abs(pinCount-knockedPins);
+                pinCount = 0;
+            }
+            
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,54 @@
+﻿using BowlingGame;
+using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        [Test]
+        public void AllRollsMissed()
+        {
+            //Arrange
+            var BowlingGame = new Game();
+            //Act
+            for (int i = 0; i <= 20; i++)
+            {
+                BowlingGame.Roll(0);
+            }
+
+            //Assert
+            Assert.AreEqual(0,BowlingGame.Score());
+        }
+        [Test]
+        public void OnePinEachRoll()
+        {
+            //Arrange
+            var BowlingGame = new Game();
+            //Act
+            for (int i = 0; i < 20; i++)
+            {
+                BowlingGame.Roll(1);
+            }
+
+            //Assert
+            Assert.AreEqual(20,BowlingGame.Score());
+        }
+        [Test]
+        public void SixteenScore()
+        {
+            //Arrange
+            var BowlingGame = new Game();
+            //Act
+            BowlingGame.Roll(13);
+            BowlingGame.Roll(3);
+            for (int i = 0; i < 18; i++)
+            {
+                BowlingGame.Roll(0);
+            }
+
+            //Assert
+            Assert.AreEqual(16,BowlingGame.Score());
+        }
+    }
+}
