3ac8217cd9a951d299b2db8d8cc6c30ac9bff17b
anisbastide <anis.bastide@upslide.net>
2020-02-17 09:06:23 +0100

    redid the kata without modifying the Roll() method.

---
+++ BowlingGame/BowlingGame/GameWithPins.cs
@@ -0,0 +1,125 @@
+﻿namespace BowlingGame
+{
+    public class GameWithPins
+    {
+        private int _score;
+        private int _rollNumber = 1;
+        private int _knockedPins;
+        private bool _strike;
+        private bool _spare;
+        private int _strikeCount;
+        private int _frameNumber=1;
+        private bool _secondStrike;
+
+        public void Roll(int pins)
+        {
+            if (_frameNumber > 10)
+            {
+                _score += pins;
+            }
+            else
+            {
+                if (_strike)
+                {
+                    Strike(pins);
+                }
+
+                if (_spare)
+                {
+                    Spare(pins);
+                }
+                RollNumber(pins);
+
+                if (_secondStrike)
+                {
+                    _score += pins;
+                }
+
+                _score += pins;
+            }
+        }
+
+        private void RollNumber(int pins)
+        {
+            if (_rollNumber == 1)
+            {
+                _knockedPins += pins;
+                if (_frameNumber > 1)
+                {
+                    SecondStrike(pins);
+                }
+                IsSpareOrStrike(pins);
+
+                if (_strike != true)
+                {
+                    _rollNumber = 2;
+                }
+            }
+            else if (_rollNumber == 2)
+            {
+                _knockedPins += pins;
+                IsSpareOrStrike(pins);
+                _rollNumber = 1;
+            }
+        }
+
+        private void IsSpareOrStrike(int pins)
+        {
+            if (_rollNumber == 1 && _knockedPins == 10)
+            {
+                _knockedPins = 0;
+                _strike = true;
+                _frameNumber++;
+            }
+            else if (_rollNumber == 2 && _knockedPins == 10)
+            {
+                _spare = true;
+                _knockedPins = 0;
+                _frameNumber++;
+            }
+            else if (_rollNumber == 2)
+            {
+                _knockedPins = 0;
+                _frameNumber++;
+            }
+        }
+
+        private void Strike(int pins)
+        {
+            if (_strikeCount == 0 && _strike)
+            {
+                _strikeCount += 1;
+                _score += pins;
+            }
+            else if (_strikeCount == 1 && _strike)
+            {
+                _strike = false;
+                _strikeCount = 0;
+                _score += pins;
+            }
+        }
+
+        private void SecondStrike(int pins)
+        {
+            if (_knockedPins == 10)
+            {
+                _secondStrike = true;
+            }
+            else
+            {
+                _secondStrike = false;
+            }
+        }
+
+        private void Spare(int pins)
+        {
+            _spare = false;
+            _score += pins;
+        }
+
+        public int Score()
+        {
+            return _score;
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTests/GameWithPinsTests.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using BowlingGame;
+
+namespace BowlingGameTests
+{
+    [TestFixture]
+    public class GameWithPinsTests
+    {
+        [Test]
+        public void ScoreIsZeroWhenAllRollsMissed()
+        {
+            //Arrange
+            var bowling = new GameWithPins();
+
+            //act
+            for (int i = 0; i < 20; i++)
+            {
+                bowling.Roll(0);
+            }
+
+            var result = bowling.Score();
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void ScoreIsTwentyWhenEveryRollKnockOnePin()
+        {
+            //Arrange
+            var bowling = new GameWithPins();
+
+            //act
+            for (int i = 0; i < 20; i++)
+            {
+                bowling.Roll(1);
+            }
+
+            var result = bowling.Score();
+
+            //Assert
+            Assert.AreEqual(20, result);
+        }
+
+        [Test]
+        public void ScoreIsSixteenWhenASpareIsDoneAndThenThreePinsAndThenAllMissed()
+        {
+            //Arrange
+            var bowling = new GameWithPins();
+
+            //act
+            bowling.Roll(3);
+            bowling.Roll(7);
+            bowling.Roll(3);
+            for (int i = 0; i < 17; i++)
+            {
+                bowling.Roll(0);
+            }
+
+            var result = bowling.Score();
+
+            //Assert
+            Assert.AreEqual(16, result);
+        }
+
+        [Test]
+        public void ScoreIsTwentyFour()
+        {
+            //Arrange
+            var bowling = new GameWithPins();
+
+            //act
+            bowling.Roll(10);
+            bowling.Roll(3);
+            bowling.Roll(4);
+            for (int i = 0; i < 16; i++)
+            {
+                bowling.Roll(0);
+            }
+
+            var result = bowling.Score();
+
+            //Assert
+            Assert.AreEqual(24, result);
+        }
+
+        [Test]
+        public void AllStrike()
+        {
+            //Arrange
+            var bowling = new GameWithPins();
+
+            //act
+            for (int i = 0; i < 12; i++)
+            {
+                bowling.Roll(10);
+                var score = bowling.Score();
+            }
+            var result = bowling.Score();
+            //Assert
+            Assert.AreEqual(300, result);
+        }
+    }
+}
